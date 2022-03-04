9644a792c1f257a1975d633324c5a983a84fedd1
DrigsGomes <rodrigo.vitor169@gmail.com>
2022-03-04 17:26:38 -0300

    update.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const resultado = document.querySelector('.result');
+const confirmar = document.querySelector('.igual');
+
+function insert(valor){
+    resultado.innerHTML += valor;
+
+}
+
+function clean(){
+    resultado.innerHTML= '';
+}
+
+function backspace(){
+    if(resultado.textContent){
+        let result =document.getElementById('resultado').innerHTML;
+        resultado.innerHTML = result.substring(0, result.length -1);
+    }
+}
